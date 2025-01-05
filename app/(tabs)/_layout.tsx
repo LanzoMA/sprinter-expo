edf87cf0ba43923a8e926050455c8de19a30dce0
LanzoMA <lanzo.ma9@gmail.com>
2025-01-05 20:31:22 +0000

    implemented navigation bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,53 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarActiveTintColor: 'white',
+        tabBarStyle: {
+          height: 60,
+          backgroundColor: 'black',
+          paddingTop: 4,
+        }
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name='home'
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name='home' color={color} />
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name='search'
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Search',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name='search' color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name='upload'
+        options={{
+          title: 'Upload',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name='upload' color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name='saved'
+        options={{
+          title: 'Saved',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name='folder' color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: 'Me',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name='user' color={color} />
         }}
       />
     </Tabs>
-  );
+  )
 }
