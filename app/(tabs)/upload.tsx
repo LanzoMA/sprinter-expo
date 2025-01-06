007667f6a933e38f4fe4ff2d5de524ecdc2b3d2a
LanzoMA <lanzo.ma9@gmail.com>
2025-01-06 18:22:00 +0000

    implemented inital layout of upload screen

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -1,9 +1,82 @@
-import { View, Text } from 'react-native';
+import { Image } from 'expo-image';
+import { View, Text, Button, Pressable, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
-export default function HomeScreen() {
+export default function UploadScreen() {
+  const [questionImage, setQuestionImage] = useState('');
+  const [markschemeImage, setMarkschemeImage] = useState('');
+
+  async function pickQuestionImage() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+    });
+
+    if (!result.canceled) {
+      setQuestionImage(result.assets[0].uri);
+    }
+  }
+
+  async function pickMarkschemeImage() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+    });
+
+    if (!result.canceled) {
+      setMarkschemeImage(result.assets[0].uri);
+    }
+  }
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View
+      style={{
+        gap: 8,
+        padding: 8,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: 'row',
+          height: 350,
+          gap: 8,
+        }}
+      >
+        <Pressable
+          style={{ flex: 1 }}
+          onPress={pickQuestionImage}
+        >
+          <Image
+            style={styles.image}
+            source={questionImage}
+            contentFit='contain'
+          />
+        </Pressable>
+        <Pressable
+          style={{ flex: 1 }}
+          onPress={pickMarkschemeImage}
+        >
+          <Image
+            style={styles.image}
+            source={markschemeImage}
+            contentFit='contain'
+          />
+        </Pressable>
+      </View>
+
+      <Text>Number of Marks</Text>
+
+      <Button title='Submit' />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  image: {
+    borderRadius: 16,
+    backgroundColor: 'white',
+    height: '100%',
+    width: '100%',
+  }
+});
