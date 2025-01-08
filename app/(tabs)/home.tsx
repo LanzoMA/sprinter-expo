66a4b143c3abeec402330fdf5904006d80b1b8e6
LanzoMA <lanzo.ma9@gmail.com>
2025-01-08 21:46:01 +0000

    added scrolling functionality

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,48 @@
-import { View, Text } from 'react-native';
+import { Image } from 'expo-image';
+import { View, FlatList, Dimensions } from 'react-native';
 
 export default function HomeScreen() {
+  const height = Dimensions.get('window').height - 60;
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View
+      style={{
+        backgroundColor: 'white',
+        flex: 1
+      }}
+    >
+      <FlatList
+        data={
+          [
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+          ]
+        }
+
+        renderItem={
+          () =>
+            <View style={{ height: height }} >
+              <Image
+                style={{
+                  flex: 1,
+                  width: '100%',
+                }}
+                source={require('@/assets/images/questions/question1.png')}
+                contentFit='contain'
+              />
+            </View>
+        }
+
+        snapToInterval={height}
+        decelerationRate={"normal"}
+        disableIntervalMomentum
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  const loggedIn = false;
+  const loggedIn = true;
 
   return <Redirect href={loggedIn ? '/(tabs)/home' : '/(auth)/login'} />
 }
