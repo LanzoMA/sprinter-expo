81bc4d90d3489c075412dbe8a791c22105e34cca
LanzoMA <lanzo.ma9@gmail.com>
2025-01-10 16:13:25 +0000

    implemented initial screens

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { useTheme, Text } from '@rneui/themed';
+import { View } from 'react-native';
 
-export default function HomeScreen() {
+export default function SavedScreen() {
+  const { theme } = useTheme();
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View
+      style={{
+        backgroundColor: theme.colors.background,
+        flex: 1,
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={{ textAlign: 'center' }}>No saved posts.</Text>
     </View>
   )
 }
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { useTheme, Text } from '@rneui/themed';
+import { View } from 'react-native';
 
-export default function HomeScreen() {
+export default function SearchScreen() {
+  const { theme } = useTheme();
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View
+      style={{
+        backgroundColor: theme.colors.background,
+        flex: 1,
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={{ textAlign: 'center' }}>Nothing to search.</Text>
     </View>
   )
 }
