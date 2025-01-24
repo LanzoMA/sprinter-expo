da96143c4660f0b302c26571d54114bf6e07938b
LanzoMA <lanzo.ma9@gmail.com>
2025-01-24 11:59:54 +0000

    fixed login error message

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,10 +1,10 @@
-import { Link, router } from "expo-router";
-import { View } from "react-native";
+import { Link, router } from 'expo-router';
+import { View } from 'react-native';
 import { Text, useTheme, Button, Input } from '@rneui/themed';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import { Snackbar } from 'react-native-paper';
-import { baseUrl } from "@/constants/base-url";
-import { storeAccessToken } from "@/constants/token-access";
+import { baseUrl } from '@/constants/base-url';
+import { storeAccessToken } from '@/constants/token-access';
 
 export default function LoginScreen() {
   const { theme } = useTheme();
@@ -15,29 +15,26 @@ export default function LoginScreen() {
   const [loading, setLoading] = useState<boolean>(false);
 
   const [message, setMessage] = useState<string>('');
-  const [visible, setVisible] = useState<boolean>(true);
-
-  useEffect(() => {
-    message !== '' ? setVisible(true) : setVisible(false);
-  }, [message])
+  const [visible, setVisible] = useState<boolean>(false);
 
   async function login() {
     setLoading(true);
 
     if (email === '') {
       setMessage('Email is not provided');
+      setVisible(true);
       setLoading(false);
       return;
     }
 
     if (password === '') {
       setMessage('Password is not provided');
+      setVisible(true);
       setLoading(false);
       return;
     }
 
-    const response = await fetch(
-      `${baseUrl}/login/`, {
+    const response = await fetch(`${baseUrl}/login/`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -48,6 +45,7 @@ export default function LoginScreen() {
 
     if (response.status !== 200) {
       setMessage('Incorrect email/password');
+      setVisible(true);
       setLoading(false);
       return;
     }
@@ -58,6 +56,7 @@ export default function LoginScreen() {
 
     if (!accessToken) {
       setMessage('Something went wrong: access token not found');
+      setVisible(true);
       setLoading(false);
       return;
     }
@@ -77,25 +76,32 @@ export default function LoginScreen() {
         gap: 32,
       }}
     >
-      <Text style={{
-        textAlign: 'center',
-        fontSize: 24,
-        fontWeight: 700,
-      }}
-      >Login</Text>
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 24,
+          fontWeight: 700,
+        }}
+      >
+        Login
+      </Text>
 
       <Input
-        autoCapitalize='none'
+        autoCapitalize="none"
         inputMode="email"
         placeholder="Email"
-        onChangeText={(email) => { setEmail(email.trim()) }}
+        onChangeText={(email) => {
+          setEmail(email.trim());
+        }}
       />
 
       <Input
         autoCapitalize="none"
         secureTextEntry
         placeholder="Password"
-        onChangeText={(password) => { setPassword(password.trim()) }}
+        onChangeText={(password) => {
+          setPassword(password.trim());
+        }}
       />
 
       <Button
@@ -113,10 +119,10 @@ export default function LoginScreen() {
       >
         <Text>No account?</Text>
         <Link
-          href='/signup'
+          href="/signup"
           style={{
             color: theme.colors.primary,
-            fontWeight: 700
+            fontWeight: 700,
           }}
         >
           Sign Up
@@ -125,10 +131,12 @@ export default function LoginScreen() {
 
       <Snackbar
         visible={visible}
-        onDismiss={() => { setVisible(false) }}
+        onDismiss={() => {
+          setVisible(false);
+        }}
       >
         <Text>{message}</Text>
       </Snackbar>
     </View>
-  )
+  );
 }
