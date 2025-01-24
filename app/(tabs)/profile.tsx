638ccbc60f15cabe63e05c9db379831718fed430
LanzoMA <lanzo.ma9@gmail.com>
2025-01-24 12:15:04 +0000

    modified profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,35 @@
-import { Tab, TabView, useTheme, Text, Button, Image, Icon } from '@rneui/themed';
+import { getUserDetails } from '@/constants/token-access';
+import {
+  Tab,
+  TabView,
+  useTheme,
+  Text,
+  Button,
+  Image,
+  Icon,
+} from '@rneui/themed';
 import { router } from 'expo-router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
 export default function ProfileScreen() {
   const { theme } = useTheme();
 
   const [tabIndex, setTabIndex] = useState(0);
+
+  const [username, setUsername] = useState<string>('');
+
+  useEffect(() => {
+    getUsername();
+  }, []);
+
+  async function getUsername() {
+    const user = await getUserDetails();
+
+    if (!user) return;
+
+    setUsername(user.username);
+  }
 
   function goToSettings() {
     router.push('/settings');
@@ -20,11 +43,11 @@ export default function ProfileScreen() {
       }}
     >
       <View style={{ padding: 8 }}>
-        <View style={{ flexDirection: 'row' }} >
+        <View style={{ flexDirection: 'row' }}>
           <View style={{ flex: 1 }} />
           <Icon
-            name='settings'
-            type='material'
+            name="settings"
+            type="material"
             size={24}
             color={theme.colors.white}
             style={{
@@ -34,23 +57,29 @@ export default function ProfileScreen() {
           />
         </View>
 
-        <View style={{ flexDirection: 'row', gap: 16 }}>
+        <View style={{ flexDirection: 'row', gap: 16, paddingVertical: 16 }}>
           <Image
             style={{
               width: 100,
               height: 100,
+              borderRadius: 1024,
             }}
-            source={{ uri: 'https://avatar.iran.liara.run/public/50' }}
+            source={{
+              uri: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
+            }}
           />
 
-          <View style={{ flex: 1, gap: 8 }}>
-            <Text>@danielbrown</Text>
-            <Text>Hi, I'm currently a year 13 studying Maths, Further Maths and Physics.</Text>
+          <View style={{ flex: 1, gap: 8, justifyContent: 'center' }}>
+            <Text style={{ paddingVertical: 8, fontWeight: 700 }}>
+              @{username}
+            </Text>
             <Button
               buttonStyle={{
                 backgroundColor: theme.colors.grey0,
               }}
-            >Edit Profile</Button>
+            >
+              Edit Profile
+            </Button>
           </View>
         </View>
       </View>
@@ -58,51 +87,45 @@ export default function ProfileScreen() {
       <Tab
         value={tabIndex}
         onChange={(tabIndex) => setTabIndex(tabIndex)}
-
         buttonStyle={{
           height: 60,
           backgroundColor: theme.colors.background,
         }}
-
         indicatorStyle={{
           backgroundColor: theme.colors.white,
         }}
-
-        variant='primary'
+        variant="primary"
       >
         <Tab.Item
           icon={{
             name: 'photo-library',
             type: 'material',
-            color: theme.colors.white
+            color: theme.colors.white,
           }}
         />
         <Tab.Item
           icon={{
             name: 'bar-chart',
             type: 'material',
-            color: theme.colors.white
+            color: theme.colors.white,
           }}
         />
         <Tab.Item
           icon={{
             name: 'trophy',
             type: 'ionicon',
-            color: theme.colors.white
+            color: theme.colors.white,
           }}
         />
       </Tab>
 
-      <TabView
-        value={tabIndex}
-        onChange={setTabIndex}
-      >
+      <TabView value={tabIndex} onChange={setTabIndex}>
         <TabView.Item
           style={{
             backgroundColor: theme.colors.background,
             width: '100%',
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
           }}
         >
           <Text style={{ textAlign: 'center' }}>No posts yet.</Text>
@@ -112,7 +135,7 @@ export default function ProfileScreen() {
             backgroundColor: theme.colors.background,
             width: '100%',
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
           }}
         >
           <Text style={{ textAlign: 'center' }}>No data recorded yet.</Text>
@@ -122,12 +145,12 @@ export default function ProfileScreen() {
             backgroundColor: theme.colors.background,
             width: '100%',
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
           }}
         >
           <Text style={{ textAlign: 'center' }}>No achievements yet.</Text>
         </TabView.Item>
       </TabView>
     </View>
-  )
+  );
 }
