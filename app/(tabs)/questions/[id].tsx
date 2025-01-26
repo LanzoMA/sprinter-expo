62ff11c800ec83525e8b9e279529b072fcd4f24c
LanzoMA <lanzo.ma9@gmail.com>
2025-01-26 11:40:46 +0000

    using pager view to swipe between question and mark scheme

--- app/(tabs)/questions/[id].tsx
+++ app/(tabs)/questions/[id].tsx
@@ -1,8 +1,9 @@
 import { Text, Icon } from '@rneui/themed';
 import { useLocalSearchParams } from 'expo-router';
-import { ScrollView, View, Dimensions } from 'react-native';
+import { View, Dimensions, Pressable } from 'react-native';
 import { Image } from 'expo-image';
 import { useEffect, useState } from 'react';
+import PagerView from 'react-native-pager-view';
 import { Question } from '@/constants/models';
 import { baseUrl } from '@/constants/base-url';
 
@@ -11,6 +12,7 @@ export default function QuestionScreen() {
   const { width } = Dimensions.get('window');
   const [question, setQuestion] = useState<Question>();
   const [favorited, setFavorited] = useState<boolean>(false);
+  const [visible, setVisible] = useState<boolean>(false);
 
   useEffect(() => {
     getQuestion();
@@ -24,58 +26,68 @@ export default function QuestionScreen() {
   }
 
   return (
-    <View style={{ width, flex: 1 }}>
-      <ScrollView
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        decelerationRate={'fast'}
-        style={{ backgroundColor: 'white', position: 'relative' }}
-      >
-        <Image
-          style={{ width }}
-          source={question?.question}
-          contentFit="contain"
-        />
-        <Image
-          style={{ width }}
-          source={question?.markScheme}
-          contentFit="contain"
-        />
-      </ScrollView>
-      <Text
+    <View style={{ width, flex: 1, backgroundColor: 'white' }}>
+      <PagerView
         style={{
-          color: 'black',
-          position: 'absolute',
-          bottom: 8,
-          left: 8,
-          fontWeight: 700,
-          fontSize: 16,
+          flex: 1,
+          position: 'relative',
         }}
       >
-        {question?.title}
-      </Text>
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 16,
-          right: 16,
-          gap: 32,
-        }}
-      >
-        <Icon color="black" name="account-circle" size={48} />
-        <Icon
-          color={favorited ? 'red' : 'black'}
-          name={favorited ? 'favorite' : 'favorite-outline'}
-          size={48}
-          onPress={() => {
-            setFavorited(!favorited);
-          }}
-        />
-        <Icon color="black" name="comment" size={48} />
-        <Icon color="black" name="download" size={48} />
-        <Icon color="black" name="ios-share" size={48} />
-      </View>
+        <Pressable style={{ flex: 1 }} onPress={() => setVisible(!visible)}>
+          <Image
+            style={{ flex: 1 }}
+            source={question?.question}
+            contentFit="contain"
+          />
+        </Pressable>
+        <Pressable style={{ flex: 1 }} onPress={() => setVisible(!visible)}>
+          <Image
+            style={{ flex: 1 }}
+            source={question?.markScheme}
+            contentFit="contain"
+          />
+        </Pressable>
+      </PagerView>
+      {visible ? null : (
+        <View>
+          <Text
+            style={{
+              color: 'black',
+              position: 'absolute',
+              bottom: 8,
+              left: 8,
+              fontWeight: 700,
+              fontSize: 16,
+            }}
+          >
+            {question?.title}
+          </Text>
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 16,
+              right: 16,
+              gap: 32,
+            }}
+          >
+            <Icon color="black" name="account-circle" size={48} />
+            <Pressable
+              onPress={() => {
+                setFavorited(!favorited);
+              }}
+            >
+              <Icon
+                color={favorited ? 'red' : 'black'}
+                name={favorited ? 'favorite' : 'favorite-outline'}
+                size={48}
+              />
+            </Pressable>
+            <Icon color="black" name="comment" size={48} />
+            <Icon color="black" name="download" size={48} />
+            <Icon color="black" name="ios-share" size={48} />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
