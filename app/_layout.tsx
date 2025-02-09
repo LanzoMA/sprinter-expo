4992e050a6c6ee62fc5d83f09e8ad95b5b58976a
LanzoMA <lanzo.ma9@gmail.com>
2025-02-09 12:54:03 +0000

    moved theme to separate file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,6 @@
-import { createTheme, ThemeProvider } from '@rneui/themed';
+import theme from '@/constants/theme';
+import { ThemeProvider } from '@rneui/themed';
 import { Stack } from 'expo-router';
-
-const theme = createTheme({
-  lightColors: {
-    primary: '#20cfa5',
-    background: '#fff',
-    surface: '#e0e0e0',
-    surfaceBright: '#3B3F46',
-    surfaceTransparent: '#3B3F46cc',
-    text: '#000',
-    red: '#D81F1F',
-    orange: '#E87B16',
-    yellow: '#FDE82B',
-    green: '#2FCA22',
-    blue: '#18A8D6',
-  },
-  darkColors: {
-    primary: '#20cfa5',
-    background: '#0f0f0f',
-    surface: '#2a2c30',
-    surfaceBright: '#3B3F46',
-    surfaceTransparent: '#3B3F46cc',
-    text: '#fff',
-    red: '#D81F1F',
-    orange: '#E87B16',
-    yellow: '#FDE82B',
-    green: '#2FCA22',
-    blue: '#18A8D6',
-  },
-  mode: 'dark',
-});
 
 const App = () => {
   return (
---
+++ constants/theme.ts
@@ -0,0 +1,33 @@
+import { createTheme } from '@rneui/themed';
+
+const commonColors = {
+  red: '#D81F1F',
+  orange: '#E87B16',
+  yellow: '#FDE82B',
+  green: '#2FCA22',
+  blue: '#18A8D6',
+};
+
+const theme = createTheme({
+  lightColors: {
+    primary: '#20cfa5',
+    background: '#fff',
+    surface: '#e0e0e0',
+    surfaceBright: '#3B3F46',
+    surfaceTransparent: '#3B3F46cc',
+    text: '#000',
+    ...commonColors,
+  },
+  darkColors: {
+    primary: '#20cfa5',
+    background: '#0f0f0f',
+    surface: '#2a2c30',
+    surfaceBright: '#3B3F46',
+    surfaceTransparent: '#3B3F46cc',
+    text: '#fff',
+    ...commonColors,
+  },
+  mode: 'dark',
+});
+
+export default theme;
