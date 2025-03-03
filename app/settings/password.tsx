7035a4462e2b6591f1ccb66bb5aba6cfad71c1a3
LanzoMA <lanzo.ma9@gmail.com>
2025-03-03 19:56:10 +0000

    added functionality to reset password page

--- app/settings/password.tsx
+++ app/settings/password.tsx
@@ -1,29 +1,108 @@
-import { View } from 'react-native';
-import { Text, useTheme } from '@rneui/themed';
+import { View, Text, useColorScheme, StyleSheet } from 'react-native';
+import { useState } from 'react';
 import SprinterTextInput from '@/components/SprinterTextInput';
 import SprinterButton from '@/components/SprinterButton';
+import baseTheme from '@/constants/base-theme';
+import Toast from 'react-native-toast-message';
+import { getAccessToken } from '@/constants/token-access';
+import { baseUrl } from '@/constants/base-url';
+import Spinner from '@/components/Spinner';
 
 export default function ChangePasswordScreen() {
-  const { theme } = useTheme();
+  const colorScheme = useColorScheme();
+
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor:
+        colorScheme === 'light'
+          ? baseTheme.light.background
+          : baseTheme.dark.background,
+      flex: 1,
+      padding: 16,
+      gap: 32,
+    },
+    title: {
+      color:
+        colorScheme === 'light' ? baseTheme.light.text : baseTheme.dark.text,
+      fontWeight: 700,
+      fontSize: 20,
+    },
+    fields: { gap: 16 },
+  });
+
+  async function submit() {
+    if (password === '' || confirmPassword === '') return;
+
+    if (password !== confirmPassword) {
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Passwords do not match',
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    const accessToken = await getAccessToken();
+
+    if (!accessToken) return;
+
+    try {
+      await fetch(`${baseUrl}/account/password`, {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ password }),
+      });
+
+      Toast.show({
+        type: 'success',
+        text1: 'Success',
+        text2: 'Successfully updated password',
+      });
+      setPassword('');
+      setConfirmPassword('');
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Something went wrong',
+      });
+    }
+
+    setLoading(false);
+  }
 
   return (
-    <View
-      style={{
-        backgroundColor: theme.colors.background,
-        flex: 1,
-        padding: 16,
-        gap: 32,
-      }}
-    >
-      <Text style={{ fontWeight: 700, fontSize: 20 }}>Change Password</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Change Password</Text>
 
-      <View style={{ gap: 16 }}>
-        <SprinterTextInput secureTextEntry placeholder="Current Password" />
-        <SprinterTextInput secureTextEntry placeholder="New Password" />
-        <SprinterTextInput secureTextEntry placeholder="Confirm New Password" />
+      <View style={styles.fields}>
+        <SprinterTextInput
+          secureTextEntry
+          placeholder="New Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <SprinterTextInput
+          secureTextEntry
+          placeholder="Confirm New Password"
+          value={confirmPassword}
+          onChangeText={(text) => setConfirmPassword(text)}
+        />
       </View>
 
-      <SprinterButton title="Reset Password" />
+      <SprinterButton title="Reset Password" onPress={submit} />
+
+      {loading && <Spinner />}
     </View>
   );
 }
